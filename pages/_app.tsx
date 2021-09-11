779b0675fe137bf97a9532b54e6b7d8c38052a68
Kunanan <kunanan.tw@gmail.com>
2021-09-11 14:47:21 +0700

    feat: Render `Header` in `_app`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import { globalCss } from '@stitches/react'
 import type { AppProps } from 'next/app'
+import Header from '../components/Header'
+import Box from '../components/layout/Box'
 
 const resetCSS = globalCss({
   'html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, main, menu, nav, output, ruby, section, summary, time, mark, audio, video':
@@ -50,6 +52,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   resetCSS()
   globalCSS()
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Box css={{ position: 'fixed', top: '0', left: '0', width: '100%' }}>
+        <Header />
+      </Box>
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
