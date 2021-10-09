eae83bd72aac981111a75fabf7fe8dad41415e1a
Kunanan <kunanan.tw@gmail.com>
2021-10-09 19:28:16 +0700

    Add font-family

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,44 +6,47 @@ import Header from '../components/Header'
 import Box from '../components/layout/Box'
 import DocsPage from '../components/DocsPage'
 
-const resetCSS = globalCss({
-  'html, body, div, span, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, a, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, b, u, i, center, dl, dt, dd, ol, ul, li, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, tr, th, td, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, main, menu, nav, output, ruby, section, summary, time, mark, audio, video':
-    {
-      margin: '0',
-      padding: '0',
-      border: '0',
-      fontSize: '100%',
-      font: 'inherit',
-      verticalAlign: 'baseline',
-    },
-  'article, aside, details, figcaption, figure, footer, header, hgroup, main, menu, nav, section':
-    {
-      display: 'block',
-    },
-  '*[hidden]': {
-    display: 'none',
-  },
-  body: {
-    lineHeight: '1',
-  },
-  'ol, ul': {
-    listStyle: 'none',
-  },
-  'blockquote, q': {
-    quotes: 'none',
-  },
-  'blockquote:before, blockquote:after, q:before, q:after': {
-    content: '',
-    // @ts-ignore
-    content: 'none',
-  },
-  table: {
-    borderSpacing: '0',
-  },
-})
-
 const globalCSS = globalCss({
+  '@font-face': [
+    {
+      fontFamily: 'Assistant',
+      src: 'url("/fonts/Assistant-Light.ttf")',
+      fontWeight: '300',
+      fontStyle: 'normal',
+      fontDisplay: 'swap',
+    },
+    {
+      fontFamily: 'Assistant',
+      src: 'url("/fonts/Assistant-Regular.ttf")',
+      fontWeight: '400',
+      fontStyle: 'normal',
+      fontDisplay: 'swap',
+    },
+    {
+      fontFamily: 'Assistant',
+      src: 'url("/fonts/Assistant-Medium.ttf")',
+      fontWeight: '500',
+      fontStyle: 'normal',
+      fontDisplay: 'swap',
+    },
+    {
+      fontFamily: 'Assistant',
+      src: 'url("/fonts/Assistant-SemiBold.ttf")',
+      fontWeight: '600',
+      fontStyle: 'normal',
+      fontDisplay: 'swap',
+    },
+    {
+      fontFamily: 'Assistant',
+      src: 'url("/fonts/Assistant-Bold.ttf")',
+      fontWeight: '700',
+      fontStyle: 'normal',
+      fontDisplay: 'swap',
+    },
+  ],
+
   '*, *::before, *::after': {
+    fontFamily: 'Assistant',
     boxSizing: 'border-box',
   },
   body: {
@@ -52,7 +55,6 @@ const globalCSS = globalCss({
 })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  resetCSS()
   globalCSS()
 
   const router = useRouter()
