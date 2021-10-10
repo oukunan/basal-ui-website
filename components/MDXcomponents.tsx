6423078d359ca55def705b58fc5e842a1f30391f
Kunanan <kunanan.tw@gmail.com>
2021-10-10 10:32:21 +0700

    Centralized MDX components

--- components/MDXcomponents.tsx
+++ components/MDXcomponents.tsx
@@ -1,7 +1,10 @@
 import Text from './Text'
+import CodeBlock from './CodeBlock'
 
 export const components = {
   h1: (props) => <Text as="h1" css={{ fontSize: '$8' }} {...props} />,
   h2: (props) => <Text as="h2" css={{ fontSize: '$6' }} {...props} />,
   h3: (props) => <Text as="h3" css={{ fontSize: '$4' }} {...props} />,
+  pre: (props) => <>{props.children}</>,
+  code: CodeBlock,
 }
