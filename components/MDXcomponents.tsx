93d6743be689b35b97953b3d166fe5ce21ab1fa7
Kunanan <kunanan.tw@gmail.com>
2021-10-11 09:56:20 +0700

    Query all heading node elements to QuickNavContent component

--- components/MDXcomponents.tsx
+++ components/MDXcomponents.tsx
@@ -3,8 +3,12 @@ import CodeBlock from './CodeBlock'
 
 export const components = {
   h1: (props) => <Text as="h1" css={{ fontSize: '$8' }} {...props} />,
-  h2: (props) => <Text as="h2" css={{ fontSize: '$6' }} {...props} />,
-  h3: (props) => <Text as="h3" css={{ fontSize: '$4' }} {...props} />,
+  h2: (props) => (
+    <Text as="h2" css={{ fontSize: '$6' }} {...props} data-heading="" />
+  ),
+  h3: (props) => (
+    <Text as="h3" css={{ fontSize: '$4' }} {...props} data-heading="" />
+  ),
   pre: (props) => <>{props.children}</>,
   code: CodeBlock,
 }
--- components/QuickNavContent.tsx
+++ components/QuickNavContent.tsx
@@ -1,28 +1,49 @@
+import { useState, useEffect } from 'react'
+
 import { styled } from '../stitches.config'
+import Box from './layout/Box'
 
-const Aside = styled('aside', {
-  position: 'sticky',
-  top: '$8',
-  height: '100%',
-  width: '220px',
-  padding: '0 $4',
-})
-
-const UnOrderList = styled('ul', {
+const ContainerUL = styled('ul', {
   margin: 0,
   padding: 0,
+  listStyle: 'none',
 })
 
+const ListItem = styled('li')
+
 export default function QuickNavContent() {
+  const [headers, setHeaders] = useState<HTMLHeadingElement[]>([])
+
+  useEffect(() => {
+    const articleHeaders: HTMLHeadingElement[] = Array.from(
+      document.querySelectorAll(`[data-heading]`)
+    )
+
+    setHeaders(articleHeaders)
+  }, [])
+
   return (
-    <Aside>
+    <Box
+      as="aside"
+      css={{
+        position: 'sticky',
+        top: '$8',
+        height: '100%',
+        width: '220px',
+        padding: '0 $4',
+      }}
+    >
       <h4>Quick Content</h4>
-      <UnOrderList>
-        <li>This is 1</li>
-        <li>This is 2</li>
-        <li>This is 3</li>
-        <li>This is 4</li>
-      </UnOrderList>
-    </Aside>
+      <ContainerUL>
+        {headers.map((item, index) => (
+          <ListItem
+            key={index}
+            css={{ paddingLeft: item.tagName === 'H3' ? '$2' : undefined }}
+          >
+            {item.textContent}
+          </ListItem>
+        ))}
+      </ContainerUL>
+    </Box>
   )
 }
